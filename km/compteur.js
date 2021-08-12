99b2eb6ab9a7de60b3de71ea9b934336a0318d37
zxperts <69358044+zxperts@users.noreply.github.com>
2021-08-12 17:29:53 +0200

    Test Photo

--- km/compteur.js
+++ km/compteur.js
@@ -221,7 +221,7 @@ function updateCookie(){
   alert('Value Modified: ');
   animateValue("kmDash", 0, arrKm[0][0], 1000);
 
-  demoFromHTML();
+  // demoFromHTML();
 
   
   }
@@ -259,3 +259,40 @@ function testCoockies(){
   document.cookie = "testcookie=1; expires=Thu, 01-Jan-1970 00:00:01 GMT";
   return enabled;
 }
+
+
+
+
+
+let camera_button = document.querySelector("#start-camera");
+let video = document.querySelector("#video");
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+let dataurl = document.querySelector("#dataurl");
+let dataurl_container = document.querySelector("#dataurl-container");
+
+camera_button.addEventListener('click', async function() {
+   	let stream = null;
+
+    try {
+    	stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    }
+    catch(error) {
+    	alert(error.message);
+    	return;
+    }
+
+    video.srcObject = stream;
+
+    video.style.display = 'block';
+    camera_button.style.display = 'none';
+    click_button.style.display = 'block';
+});
+
+click_button.addEventListener('click', function() {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   	let image_data_url = canvas.toDataURL('image/jpeg');
+    
+    dataurl.value = image_data_url;
+    dataurl_container.style.display = 'block';
+});
