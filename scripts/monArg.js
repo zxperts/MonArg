e56aa1dc043c3b2d4ef2b4040029081f2b4fc0d6
zxperts <zxperts@hotmail.com>
2023-01-12 17:14:07 +0100

    Link Correction

--- scripts/monArg.js
+++ scripts/monArg.js
@@ -414,7 +414,6 @@ function findLongestWord(str) {
     var longestWord = str.split(' ').reduce(function(longest, currentWord) {
         return currentWord.length > longest.length ? currentWord : longest;
     }, "");
-    return longestWord;
 }
 
 
